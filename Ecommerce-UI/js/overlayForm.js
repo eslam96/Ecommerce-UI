208d87a0ae961f22b6fbfd8d5af47e6156d8b3ef
eslam96 <eslammamdouh25@yahoo.com>
2022-04-27 12:50:31 +0200

    removing unused scripts

--- Ecommerce-UI/js/overlayForm.js
+++ Ecommerce-UI/js/overlayForm.js
@@ -1,16 +1,3 @@
-// let btn1 = document.querySelector(".btn1");
-// let btn2 = document.querySelector(".btn2");
-
-// let overlay = document.querySelector('.overlayWindow');
-
-// btn1.addEventListener("click", function(){
-//     overlay.style.width = "100%";
-// });
-
-// btn2.addEventListener("click", function(){
-//     overlay.style.width = "100%";
-// });
-
 function openOverlay() {
   document.querySelector(".overlayWindow").style.width = "100%";
 }
